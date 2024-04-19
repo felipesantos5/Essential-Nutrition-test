8b4a8e5e857869852a353daef206ade9f2b6cb37
Felipe Santos <felipesantosmarcelino2004@gmail.com>
2024-04-19 12:09:00 -0300

    fix: deleting js

--- js/darkmode.js
+++
@@ -1,7 +0,0 @@
-const btnDarkModeToggle = document.getElementById("btn-dark-mode-toggle");
-const html = document.querySelector("html");
-
-btnDarkModeToggle.onclick = function () {
-  html.classList.toggle("dark-mode");
-  console.log("test");
-};
